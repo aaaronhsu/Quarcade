3d3e6ffc39d0f9d6dffa644d3bd21e74d594e7fd
ElizaKnapp <eknapp20@stuy.edu>
2021-02-20 18:17:40 -0500

    router done

--- server/routes/roomCodes.js
+++ server/routes/roomCodes.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const router = express.Router();
+//imports schema from models
 const RoomCode = require("../models/roomCode");
+
+//pushing a room code to mongo
+router.post("/", async (req, res) => {
+  //create the information
+  const roomCode = new RoomCode({
+    RoomCode: req.body.RoomCode
+  });
+  //add the information
+  try {
+    const newRoomCode = await roomCode.save();
+    res.status(201).json(newRoomCode); //201 is a successfully created code
+  } catch (error) {
+    res.status(400).json({ message: error }); //400 errors are when user gives bad data
+  }
+});
+
+module.exports = router;
