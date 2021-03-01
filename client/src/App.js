031a0efea9d2250de6868d6baa490cf274891be9
Arthur Liang <arthurlianga@gmail.com>
2021-03-01 00:46:15 -0500

    edit App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './App.css';
+import Foo from './Button'
+import NameForm from './Form'
 
-class App extends Component {
+class Full extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      isToggleOn: true
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={() => this.handleClick()}>
+          Toggle State {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+        {<Foo />}
+        {<NameForm />}
       </div>
     );
   }
 }
 
-export default App;
+export default Full;
