f0d26eea4a730f2483d7662a28d694431bb93306
Arthur Liang <arthurlianga@gmail.com>
2021-03-04 23:31:15 -0500

    included new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import RoomCode from './RoomCode.js'
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      isToggleOn: true
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <h1>Quarcade</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <button onClick={() => this.handleClick()}>
+          Log {this.state.isToggleOn ? 'On' : 'Out'}
+        </button>
+        {<RoomCode />}
       </div>
     );
   }
