a78a4b28f382783edadd0494d1d40787d9e86635
ElizaKnapp <eknapp20@stuy.edu>
2021-03-17 21:46:11 -0400

    in the middle of the state

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,9 @@ import socket from "../../ClientSocket.js";
 class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: []
+    };
   }
   render() {
     return (
