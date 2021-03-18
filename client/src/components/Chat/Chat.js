9ff51666be45ac4ad68cf065717f308384a101d2
ElizaKnapp <eknapp20@stuy.edu>
2021-03-17 22:19:35 -0400

    very superficial front end chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,13 +5,48 @@ class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      messages: [],
+      message: "",
+      sender: ""
     };
   }
+
+  handleChange = event => {
+    const message = event.target.value;
+
+    this.setState({
+      message: message
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.state.messages.push(this.state.message);
+
+    this.setState({ message: "" });
+  };
+
   render() {
     return (
       <div>
         <h1>Chat!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Send Message:
+            <input name="message" type="text" value={this.state.message} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h3>See Messages Below:</h3>
+        <div>
+          {this.state.messages.map(message => (
+            <small>
+              {message}
+              <br></br>
+            </small>
+          ))}
+        </div>
       </div>
     );
   }
