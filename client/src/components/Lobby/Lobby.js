6699b33c3adacc8d50ac7b314a782e51d1caa791
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-19 18:53:26 -0400

    removed player name changing

--- client/src/components/Lobby/Lobby.js
+++ client/src/components/Lobby/Lobby.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ChooseGame from './ChooseGame.js'
 import Players from './Players.js';
-import NameChange from './NameChange.js';
 import ReadyButton from './ReadyButton.js';
 
 class Lobby extends React.Component {
@@ -9,7 +8,6 @@ class Lobby extends React.Component {
     return (
       <div>
         <ChooseGame />
-        <NameChange />
         <Players />
       </div>
     )}
