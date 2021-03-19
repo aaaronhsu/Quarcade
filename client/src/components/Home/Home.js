48d4fab321b5742391c55b2298a0b68f0233bbd2
Aaron Hsu <pufflewizzy@gmail.com>
2021-03-19 18:55:51 -0400

    removed print sockets button

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,27 +9,11 @@ class Home extends React.Component {
     super(props);
   }
 
-  requestSocketInfo = () => {
-    socket.emit("requestPlayersInRoom");
-  }
-
-  renderConnectedSocketsButton = () => {
-    return (
-      <div>
-        <button onClick = {() => this.requestSocketInfo()}>
-          Print rooms this socket is in (to server terminal)
-        </button>
-      </div>
-    );
-  };
-
   render() {
     return (
       <div>
         <JoinRoom />
         <CreateRoom />
-
-        {this.renderConnectedSocketsButton()}
       </div>
     )}
 }
