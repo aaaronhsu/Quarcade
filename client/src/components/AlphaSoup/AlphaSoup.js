ccc855121217cd9d77233dc92a656d2e5a659d4c
Arthur Liang <arthurlianga@gmail.com>
2021-03-19 15:46:43 -0400

    ready for next tile button in alphasoup

--- client/src/components/AlphaSoup/AlphaSoup.js
+++ client/src/components/AlphaSoup/AlphaSoup.js
@@ -5,14 +5,36 @@ class AlphaSoup extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      ready: false,
+      tileCount : 0,
+    }
+  }
+
+  // handles clicking the ready for next tile button
+  handleClick = event => {
+    this.setState({
+      ready: !this.state.ready
+    });
+    console.log('Player ready for next tile');
+
+    // .put click to backend to update number of users who have readied up
+    // need to connect with backend database and reveal next tile once everyone readies up
   }
 
   render() {
     return (
       <div>
-
+        <h2>Tiles left: {this.state.tileCount}</h2>
+        {this.state.ready ? null :
+          <button onClick={this.handleClick}>
+            <label>
+              Ready for next tile?
+            </label>
+          </button>}
       </div>
-    )}
+    );
+  }
 }
 
 export default AlphaSoup;
