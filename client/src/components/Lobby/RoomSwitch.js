a2cb84b5b70dc4cf69f7ec899bdc395577a7c5bf
Eliza <ekoknapp@gmail.com>
2021-04-24 10:35:53 -0400

    removed console logs

--- client/src/components/Lobby/RoomSwitch.js
+++ client/src/components/Lobby/RoomSwitch.js
@@ -15,14 +15,14 @@ class RoomSwitch extends React.Component {
   // ------------------------------------ Socket.io ------------------------------------
 
   componentDidMount() {
-    console.log("roomswitch component mounted");
+    // console.log("roomswitch component mounted");
     // when the component mounts, request an auto switch
     // IMPORTANT NOTICE ALL CODE READERS (aaron lol) THIS IS WHERE IT SWITCHES TO ALPHASOUP IN THE DATABASE
     // records the room the socket is in
     clientSocket.emit("reqSocketRoomDatabaseSwitch");
     clientSocket.on("recSocketRoomDatabaseSwitch", (room) => {
       this.setState({timesRun: this.state.timesRun + 1});
-      console.log(room);
+      // console.log(room);
       this.makeSwitch(room);
     });
   }
@@ -37,7 +37,7 @@ class RoomSwitch extends React.Component {
 
   // takes the room from socket and requests the data
   async makeSwitch(room) {
-    console.log("made it to the actual switch");
+    // console.log("made it to the actual switch");
     try {
       await Axios.get(`http://localhost:5000/homeLobby/${room}`).then(
         res => {
@@ -54,15 +54,14 @@ class RoomSwitch extends React.Component {
 
   // adds a clone of the room from homelobbies to alphasoup
   async addRoom(roomInfo) {
-    // TODO: change the amount of letters left
-    // TODO: change the amount of letters that begin on board IF players are greater than 4
+    // change the amount of letters left
     const userNum = roomInfo.users.length;
     const lettersLeft = userNum * 15;
+    // change the amount of letters that begin on board IF players are greater than 4
     var startLetters = 4;
     if (userNum > 4) {
       startLetters = userNum;
     }
-
     if (this.state.timesRun <= 1) {
       try {
         // posts the data to the alphasoup database
