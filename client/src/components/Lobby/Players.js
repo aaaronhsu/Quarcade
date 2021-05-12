97fe89221e4c281033ae3b126b4d65f9b8ea6509
Aaron Hsu <aaronhsu4606@gmail.com>
2021-05-11 23:16:18 -0400

    players component

--- client/src/components/Lobby/Players.js
+++ client/src/components/Lobby/Players.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NameSwitch from './NameSwitch.js'
 
 import clientSocket from "../../ClientSocket.js";
+import './Players.css';
 
 class Players extends React.Component {
   constructor(props) {
@@ -39,8 +40,9 @@ class Players extends React.Component {
   
   render() {
     return (
-      <div>
-        <h1>List of Players in {this.state.room}: (click your name to change it)</h1>
+      <div class="players">
+
+        <h1 class="players-title">Players</h1>
 
         {
           this.state.players.map(player => (
@@ -50,6 +52,8 @@ class Players extends React.Component {
             />
           ))
         }
+
+        <p class="players-roomcode">Room Code: <span class="players-code">{this.state.room}</span></p>
 
       </div>
     );
