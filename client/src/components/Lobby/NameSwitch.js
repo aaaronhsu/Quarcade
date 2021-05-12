b5000ce456ac5a0337ba7ed70b5cc4e4738661c6
Aaron Hsu <aaronhsu4606@gmail.com>
2021-05-12 01:38:59 -0400

    players can only change their name to <=15 characters long

--- client/src/components/Lobby/NameSwitch.js
+++ client/src/components/Lobby/NameSwitch.js
@@ -65,6 +65,8 @@ class NameSwitch extends React.Component {
     // adds to the database, for later games
     this.addName();
 
+    if (this.state.currentName.length > 15) return;
+
     // causes the backend to change client.username
     clientSocket.emit("changeUsername", this.state.currentName);
     // now that the username is changed, emit the request to repull users
